fa143e07f0c47e124953af0ebaac67f6e657b4c6
MarvelMathesh <123v.mathesh@gmail.com>
2023-11-26 18:05:35 +0530

    Improve Code quality (part-1)

--- rpi.py
+++ rpi.py
@@ -2,27 +2,38 @@ import RPi.GPIO as GPIO
 import time
 import Adafruit_DHT
 
-# Init
+# Set GPIO pin numbering mode and set GPIO pins for moisture sensor, relay, and temperature/humidity sensor
 GPIO.setmode(GPIO.BCM)
-# Moisture sensor
-GPIO.setup(18, GPIO.IN)
-# Relay (water pump)
-GPIO.setup(4, GPIO.OUT)
-# Temperature and Humidity sensor
-GPIO.setup(22, GPIO.IN)
+GPIO.setup(18, GPIO.IN)  # Moisture sensor
+GPIO.setup(4, GPIO.OUT)  # Relay
+GPIO.setup(22, GPIO.IN)  # Temperature/humidity sensor
+
+# Define the sensor type as DHT11
 sensor = Adafruit_DHT.DHT11
 
 while True:
+    # Read the temperature and humidity from the DHT11 sensor
     humidity, temperature = Adafruit_DHT.read_retry(sensor, 22)
-    moisture_value = GPIO.input(18)
-    moisture_percentage = (moisture_value / 1023) * 100
+
+    # Read the analog moisture sensor value
+    moisture_value = GPIO.input(18)  # Read analog value from moisture sensor
+
+    # Convert analog moisture sensor value to a percentage
+    moisture_percentage = (moisture_value / 1023) * 100  # Normalize analog value to 0-100% range
+
+    # Check if the soil moisture is below the threshold (50%)
     if moisture_percentage < 50:
+        # Turn on the water pump
         GPIO.output(4, GPIO.HIGH)
         time.sleep(3)
+
+        # Turn off the water pump
         GPIO.output(4, GPIO.LOW)
-        # Print the temperature and humidity values
-        print("Temperature: " + str(temperature) + "C")
-        print("Humidity: " + str(humidity) + "%")
-        print("Moisture: " + str(moisture_percentage) + "%")
-    else:
-        time.sleep(1)
+
+    # Print the temperature, humidity, and moisture percentage values
+    print("Temperature: " + str(temperature) + "C")
+    print("Humidity: " + str(humidity) + "%")
+    print("Moisture: " + str(moisture_percentage) + "%")
+
+    # Wait for 1 second before checking again
+    time.sleep(1)
