9ab557320fa4aa9c24e98ff208b51b13946801db
MarvelMathesh <123v.mathesh@gmail.com>
2023-11-30 17:44:05 +0530

    Implement Trigger Value & Graph

--- app.py
+++ app.py
@@ -22,6 +22,8 @@ GPIO.setup(22, GPIO.IN)  # Temperature/humidity sensor
 sensor = Adafruit_DHT.DHT11
 moisture_percentage = 0
 pump_status = False  # Initialize pump status
+pump_trigger = 60  # Default pump trigger level
+moisture_history = []
 
 def read_sensor_data():
     global moisture_percentage
@@ -34,11 +36,16 @@ def read_sensor_data():
     # Read the temperature and humidity from the DHT11 sensor
     humidity, temperature = Adafruit_DHT.read_retry(sensor, 22)
 
+    # Update moisture history
+    moisture_history.append(moisture_percentage)
+    if len(moisture_history) > 10:  # Keep only the last 10 data points for the graph
+        moisture_history.pop(0)
+
     return temperature, humidity, moisture_percentage
 
 def control_water_pump():
     global pump_status
-    if moisture_percentage < 50:
+    if moisture_percentage < pump_trigger:
         # Turn on the water pump
         GPIO.output(4, GPIO.HIGH)
         pump_status = True
@@ -53,6 +60,7 @@ def background_thread():
         control_water_pump()
         socketio.emit('sensor_update', {'temperature': temperature, 'humidity': humidity, 'moisture': moisture})
         socketio.emit('pump_status', {'pump_status': pump_status})
+        socketio.emit('moisture_history', {'moisture_history': moisture_history})
         time.sleep(5)  # Update every 5 seconds
 
 @app.route('/')
@@ -63,6 +71,11 @@ def index():
 def handle_connect():
     print('Client connected')
     Thread(target=background_thread).start()
+
+@socketio.on('set_pump_trigger')
+def handle_set_pump_trigger(data):
+    global pump_trigger
+    pump_trigger = int(data['pump_trigger'])
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0')
